e74476b5b1072a2e89e3eec20fd7d876a8f37c35
rarrissime <blavknkatha24@gmail.com>
2022-07-05 16:11:41 -0700

    functions

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - calls other function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int _islower(int c);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - calls other function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int _abs(int);
+	return (0);
+}
