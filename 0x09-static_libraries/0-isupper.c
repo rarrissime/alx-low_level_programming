6b14e52ac2946631f00afa901e2dcbae4e2e4e9e
rarrissime <blavknkatha24@gmail.com>
2022-07-22 11:27:28 -0700

    static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - print uppercase
+ * @c: print c
+ * Return: 1 if uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar ('\n');
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - print lowercase
+ * @c: print c
+ * Return: always 0(success)
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar ('\n');
+}
